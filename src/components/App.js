599be62061a778d73b89796f48827f7c15eadab4
Akash jobanputra <akash.jobanputra@hotmail.com>
2019-09-03 18:02:02 +0530

    Video 1

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrelloList from "./TrelloList";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello World!</h2>
+      <TrelloList title="Test"/>
+    </div>
+  );
+}
+
+export default App;
